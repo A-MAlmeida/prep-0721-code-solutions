f52347c6b6f1f6eba5697f77271985f4bf630e08
Andrew <mister_andrew1@yahoo.com>
2021-08-12 20:46:48 -0700

    Completed prep-javascript-for-loops with proper formatting

--- prep-javascript-for-loops/main.js
+++ prep-javascript-for-loops/main.js
@@ -4,23 +4,20 @@ let firstLoop = '';
 
 for (let i = 0; i < 10; i++) {
   firstLoop = firstLoop + i;
-
+  console.log(i);
 }
-
-console.log(firstLoop);
 
 let secondLoop = '';
 
 for (let i = 0; i < 20; i += 2) {
   secondLoop = secondLoop + i;
+  console.log(i);
 }
-
-console.log(secondLoop);
 
 let thirdLoop = 'Time till explosion: ';
 
 for (let i = 100; i > 0; i--) {
   thirdLoop = thirdLoop + i;
-}
+  console.log(i);
 
-console.log(thirdLoop);
+}
